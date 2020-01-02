2b034ea27cc7be64de493436085a3003eeeb2608
Adrien Dulong <adrien.dulong@gmail.com>
2020-01-02 14:40:55 +0100

    netlify deploy

--- static/js/usafathom-analytics.js
+++ static/js/usafathom-analytics.js
@@ -1,13 +1,11 @@
-<script>
 (function(f, a, t, h, o, m){
-a[h]=a[h]||function(){
-(a[h].q=a[h].q||[]).push(arguments)
+  a[h]=a[h]||function(){
+   (a[h].q=a[h].q||[]).push(arguments)
 };
 o=f.createElement('script'),
 m=f.getElementsByTagName('script')[0];
 o.async=1; o.src=t; o.id='fathom-script';
 m.parentNode.insertBefore(o,m)
-})(document, window, 'https://cdn.usefathom.com/tracker.js', 'fathom');
+})(document, window, '//cdn.usefathom.com/tracker.js', 'fathom');
 fathom('set', 'siteId', 'NOCCJNRF');
 fathom('trackPageview');
-</script>
